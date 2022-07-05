940b609bd06c15636f735164b9062786bf4c5232
Tank-rp <20022678@myrp.edu.sg>
2022-07-05 12:52:14 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -1,6 +1,9 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Title</title>
+  <title>Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Hello World">
 </head>
 <body>
   Hello Worlds!
